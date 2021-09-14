f1a9dcfc9124ec796aca5f6375c7ec3c8bdd45dd
Marko Jozic <marko.jozic.1990@gmail.com>
2021-09-14 21:32:55 +0200

    adding the Interface

--- libs/libs/api/util-interface/src/lib/libs-api-util-interface.spec.ts
+++
@@ -1,7 +0,0 @@
-import { libsApiUtilInterface } from './libs-api-util-interface';
-
-describe('libsApiUtilInterface', () => {
-  it('should work', () => {
-    expect(libsApiUtilInterface()).toEqual('libs-api-util-interface');
-  });
-});
--- libs/libs/api/util-interface/src/lib/libs-api-util-interface.ts
+++
@@ -1,3 +0,0 @@
-export function libsApiUtilInterface(): string {
-  return 'libs-api-util-interface';
-}
